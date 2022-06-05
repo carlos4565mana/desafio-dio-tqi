dd399d0338065c14d87ec349d9241c2c6c021f62
carlos4565mana <carloscal61@gmail.com>
2022-06-05 16:28:15 -0300

    criando as funcoes flipCard, unFlipCard, removeClick, checkMatch

--- script.js
+++ script.js
@@ -23,10 +23,12 @@ const cardsArray= [
 
 ]
 
+let locked = false
+let firstClick, secondClick
+
 const grid = document.querySelector('#section-game')
 
-function start(){
-
+function creatHtml(){
   const createHtml = cardsArray.forEach(
     (item) => {
       const card = document.createElement('div')
@@ -48,5 +50,49 @@ function start(){
       card.appendChild(cardBack)
     }
   )
+
+  const cards = document.querySelectorAll('.memory-card')
+  cards.forEach(card => card.addEventListener('click',flipCard))
+
+}
+
+function flipCard(){
+  if(locked)return false
+  this.classList.add('flip')
+  if(!firstClick){
+    firstClick = this
+    firstClick.removeEventListener('click', flipCard)
+    return false
+  }
+  secondClick = this
+  checkMatch()
+}
+
+function checkMatch(){
+  let isMatch = firstClick.dataset.name === secondClick.dataset.name ? true : false
+  isMatch ? removeClick() : unFlipCard()
+
+}
+
+function unFlipCard(){
+  locked = true
+  setTimeout(() => {
+    firstClick.classList.remove('flip')
+    firstClick.addEventListener('click', flipCard)
+    secondClick.classList.remove('flip')
+
+    locked = false
+    firstClick = null
+    secondClick = null
+
+  },500)
+}
+
+function removeClick(){
+
+  firstClick.removeEventListener('click', flipCard)
+  secondClick.removeEventListener('click', flipCard)
+
+  [locked,firstClick,secondClick] = [false,null,null]
 
 }
